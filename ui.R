16fca89cd0025c6a262d6dac275ae5118289f113
doubletang <doubletang@users.noreply.github.com>
2015-08-22 22:18:00 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,40 +1,64 @@
 shinyUI(fluidPage(
-
-  titlePanel("PN"),
+  
+  #titlePanel("Plate Normalizer"),
   
   fluidRow(
     
-    column(
-      
-      5,
-      
-      fileInput('load_conc', 'Load conc'),
-      
-      rHandsontableOutput('conc_tbl'),
-      
-      br(), br(),
-      
-      rHandsontableOutput('vol_tbl')
-      
+    column(3),
+    
+    column(6,
+           
+           titlePanel("Plate Normalizer"),
+           
+           
+           tabsetPanel(
+             
+             tabPanel(
+               
+               'Source plate',
+               
+               #hr(),
+               
+               h3('Concentration'),
+               
+               fileInput('load_conc', 'Load conc'),
+               
+               rHandsontableOutput('conc_tbl'),
+               
+               br(), hr(),
+               
+               h3('Volumn'),
+               
+               numericInput('uni_vol', 'Volumn', 50, 20, 1000, 1, width='100px'),
+               
+               actionButton('fill', 'Fill all'),
+
+               br(), br(),
+               
+               rHandsontableOutput('vol_tbl')
+               
+             ),
+             
+             
+             tabPanel(
+               
+               'Work plan',
+               
+               hr(),
+               
+               uiOutput('start_btn'),
+               
+               dataTableOutput('work_plan_DT')
+               
+             )
+             
+           ) 
+           
+           
     ),
     
-    column(
-      
-      7,
-      
-      h4('Work Plan'),
-      
-      uiOutput('start_btn'),
-      
-      dataTableOutput('work_plan_DT')
-      
-    )
-    
-    
+    column(3)
     
   )
   
-  
-  
-
 ))
